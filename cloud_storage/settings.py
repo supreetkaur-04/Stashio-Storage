89d836373aeff47d092459672559a47aa5510abb
Supreet Kaur <supreetkaur4154@gmail.com>
2024-11-10 10:17:11 +0530

    Updated file upload form validation

--- cloud_storage/settings.py
+++ cloud_storage/settings.py
@@ -137,3 +137,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'file_list'
--- cloud_storage/urls.py
+++ cloud_storage/urls.py
@@ -18,11 +18,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from storage import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Admin URL
+    path('login/', auth_views.LoginView.as_view(), name='login'),  # Login URL
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('auth/', include('storage.urls')), 
     path('', views.home, name='home'), 
-    path('admin/', admin.site.urls),
 ]
 
